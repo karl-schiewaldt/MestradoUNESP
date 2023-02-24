f445c3780bd06de8d9a219242ae99dc602569d5a
Karl Schiewaldt <karl.schiewaldt@gmail.com.br>
2023-02-24 17:29:20 -0300

    adicionando bases de dados

--- script.R
+++ script.R
@@ -1,1 +1,11 @@
 library(tidyverse)
+library(R.matlab)
+
+data_uhf <- readMat('uhf_data_r.mat')
+data_acust <- readMat('acustico_data_r.mat')
+
+df_uhf <- data.frame(data_uhf$data.UHF)
+df_acust <- data.frame(data_acust$data.Acustico)
+
+Fs_uhf <- data_uhf$Fs[1]
+Fs_acust <- data_acust$Fs[1]
